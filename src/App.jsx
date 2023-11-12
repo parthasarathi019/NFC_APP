2d4bd14639b43037b60079a5a716eb7bfeddf8e7
nahidhasan141400 <nahidhasan141400@gmail.com>
2023-11-13 00:36:58 +0600

    🩹

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Theme } from "@radix-ui/themes";
 import { Flex, Text, Button } from "@radix-ui/themes";
-import { ThemeProvider } from "./theme/ThemeProvider";
+import { ThemeProvider } from "@/theme/ThemeProvider";
+
 function App() {
   return (
     <ThemeProvider defaultTheme="light">
@@ -12,7 +13,7 @@ function App() {
         scaling="95%"
       >
         <Flex direction="column" gap="2">
-          <Text>Hello from Radix Themes :)</Text>
+          <Text className="font-mono">Hello from Radix Themes :)</Text>
           <Button>Let&apos;s go</Button>
         </Flex>
       </Theme>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve("src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/ui": "http://localhost:5000",
+    },
+  },
   plugins: [react()],
-})
+});
