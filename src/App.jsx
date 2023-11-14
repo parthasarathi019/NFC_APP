fd507950d88cb434e73e08fbcf6d5642aa63e396
mdmahafuzur47 <mdmahafuzur47@gmail.com>
2023-11-14 16:08:09 +0600

    login page done

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,43 @@ import { ThemeProvider } from "@/theme/ThemeProvider";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Private } from "./routes/Private";
 import AdminProvider from "./context/Admin.auth";
+import Login from "./components/login/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AdminProvider>
-        <ThemeProvider defaultTheme="light">
-          <Theme
-            accentColor="amber"
-            appearance="inherit"
-            grayColor="sand"
-            radius="large"
-            scaling="95%"
-          >
-            <Routes>
-              <Route path="/" element={<Navigate to="/dashboard" />} />
-              <Route path="/dashboard" element={<Private to="/login" />}>
-                {/* index route of Dashboard  */}
-                <Route index element={<p>hallo</p>} />
-                <Route path="*" element={<Navigate to="/dashboard" />} />
-              </Route>
+    <div className="w-full relative min-h-screen overflow-hidden px-2">
+      {/* background element */}
+      <div className="bg-ball anim-float w-64 h-64 absolute -top-28 z-[-1] rounded-full"></div>
+      <div className="bg-ball anim-float w-64 h-64 absolute top-[500px] z-[-1] rounded-full -right-20"></div>
+      {/* background element */}
+      <BrowserRouter>
+        <AdminProvider>
+          <ThemeProvider defaultTheme="light">
+            <Theme
+              accentColor="amber"
+              appearance="inherit"
+              grayColor="sand"
+              radius="large"
+              scaling="95%"
+            >
+              <Routes>
+                <Route path="/" element={<Navigate to="/dashboard" />} />
+                <Route path="/dashboard" element={<Private to="/login" />}>
+                  {/* index route of Dashboard  */}
+                  <Route index element={<p>hallo</p>} />
+                  <Route path="*" element={<Navigate to="/dashboard" />} />
+                </Route>
 
-              <Route path="*" element={<>page not exist</>} />
-              <Route path="/signin" element={<h1>Login page</h1>} />
-              <Route path="/sighup" element={<h1>Login page</h1>} />
-            </Routes>
-          </Theme>
-        </ThemeProvider>
-      </AdminProvider>
-    </BrowserRouter>
+                <Route path="*" element={<>page not exist</>} />
+                <Route path="/signin" element={<Login />} />
+                <Route path="/sighup" element={<h1>Login page</h1>} />
+              </Routes>
+            </Theme>
+          </ThemeProvider>
+        </AdminProvider>
+      </BrowserRouter>
+      {/* <div className="h-72"></div> */}
+    </div>
   );
 }
 
